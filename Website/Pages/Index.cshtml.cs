168b705ac74bf8fd8bfe48333ad2e55d7b1a584c
bamonty05 <42482171+bamonty05@users.noreply.github.com>
2020-11-05 18:06:47 -0600

    Completed Website Milestone 2

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -5,6 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using BleakwindBuffet.Data.Menu;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data;
 
 namespace Website.Pages
 {
@@ -17,9 +23,59 @@ namespace Website.Pages
             _logger = logger;
         }
 
+        /// <summary>
+        /// The items to display on the index page
+        /// </summary>
+        public IEnumerable<IOrderItem> Items { get; protected set; }
+
+        /// <summary>
+        /// The current search terms
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// The categories
+        /// </summary>
+        [BindProperty]
+        public string[] Category { get; set; }
+
+        /// <summary>
+        /// Current min calories
+        /// </summary>
+        [BindProperty]
+        public uint? CalorieMin { get; set; }
+
+        /// <summary>
+        /// Current max calories
+        /// </summary>
+        [BindProperty]
+        public uint? CalorieMax { get; set; }
+
+        /// <summary>
+        /// Current min price
+        /// </summary>
+        [BindProperty]
+        public double? PriceMin { get; set; }
+
+        /// <summary>
+        /// Current max price
+        /// </summary>
+        [BindProperty]
+        public double? PriceMax { get; set; }
+
         public void OnGet()
         {
+            Items = Menu.FullMenu();
+        }
 
+        public void OnPost()
+        {
+            Items = Menu.FullMenu();
+            Items = Menu.Search(Items, SearchTerms);
+            Items = Menu.FilterByCategory(Items, Category);
+            Items = Menu.FilterByCalories(Items, CalorieMin, CalorieMax);
+            Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
         }
     }
 }
